d1292e7f945358a7bbece4258d17847f23cb4fa8
Alexander Guzman <a.guzman@costamar.com>
2016-07-05 18:03:28 -0500

    SOAP Server test

---
+++ serverSOAP.js
@@ -0,0 +1,26 @@
+"use strict";
+
+var http = require('http');
+var soap = require('soap');
+var helloService = {
+    Hello_Service: {
+        Hello_Port: {
+            sayHello: function(args) {
+                console.log(args.firstName.$value);
+                return {
+                    greeting: "Hello!!!"+ " " + args.firstName.$value
+                };
+            }
+        }
+    }
+};
+
+var xml = require('fs').readFileSync('resources/globalweather.wsdl', 'utf8');
+
+var server = http.createServer(function(request,response) {
+    response.end("404: Not Found: "+request.url);
+});
+
+server.listen(8000);
+
+soap.listen(server, '/wsdl', helloService, xml);
