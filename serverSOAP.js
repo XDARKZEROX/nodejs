0d83cdecb6290a910b0e2446dfb06e77f50a9e27
Alexander Guzman <a.guzman@costamar.com>
2016-07-11 13:53:18 -0500

    prueba para deployar web service soap

--- serverSOAP.js
+++ serverSOAP.js
@@ -18,10 +18,14 @@ var helloService = {
 
 var xml = require('fs').readFileSync('resources/globalweather.wsdl', 'utf8');
 
-var server = http.createServer(function(request,response) {
-    response.end("404: Not Found: "+request.url);
+var server = http.createServer(function(req, res) {
+    res.end('testing');
 }).listen(5000);
 
-//server.listen(5000);
 
-soap.listen(server, '/wsdl', helloService, xml);
+/*
+var server = http.createServer(function(request,response) {
+    response.end("404: Not Found: "+request.url);
+}).listen(5000);*/
+
+soap.listen(server, '/testing', helloService, xml);
